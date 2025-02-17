3197c0cf9306115f2671821fb8fdb50e9da7ce61
Kristian <kris@gmail.com>
2025-02-17 14:12:18 -0800

    Modulos y prueba

---
+++ README.md
@@ -0,0 +1,2 @@
+# bill_bot
+Un bot en Python que manda mensajes de WhatsApp automáticamente, como si Bill hackeara la realidad.  
---
+++ bill_bot.py
@@ -0,0 +1,49 @@
+#@Kristian Martinez Colina | Programacion Python
+"----"
+"""Bill Bot: Mini-Proyecto  
+Un bot en Python que manda mensajes de WhatsApp automáticamente, como si Bill hackeara la realidad.  
+
+🟢 Guía en la descripción 🟢  
+"""
+"""Parte 3.1: ¿Como Subir un proyecto a Github?
+Bill subirá a su GitHub (o libro mágico) las modificaciones del Bill Bot o el hechizo para enviar mensajes por WhatsApp.
+
+"""
+"""Pasos: 
+1. Crear repositorio (libro mágico) en GitHub.
+2. Configurar Git (datos del hechicero o Bill).
+3. Subir proyecto (hechizos).
+"""
+
+## -> Parte 2: Automatización de WhatsApp con Selenium ##->
+from selenium import webdriver
+# 🟢 Sub modulos
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+# 🟢 Configuramos el ChromeDriver (el hechizo para controlarlo)
+from webdriver_manager.chrome import ChromeDriverManager
+
+# 🟢 Herramientas clave para interactuar con la web
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException  
+
+# 🟢 Jugamos con el tiempo y la realidad
+import time  # Pausas entre acciones
+import urllib.parse  # Modifica URLs sin errores
+
+## -> Parte 3: Que es Github, comandos básicos de github
+
+
+
+
+
+
+
+
+
+
+
+
